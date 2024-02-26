f1a8c3b07e64fb727627705691e75bb57c0e0cdb
Raajath <rajathn02@gmail.com>
2024-02-26 12:49:39 +0530

    optimised insert function

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-//call CRUD functions here
+const mongoose = require('mongoose');
+const {insertAll}=require('./insertFunctions');
+
+
+mongoose.connect('mongodb://localhost/EvModelling')
+    .then(async function() {
+      console.log('Connected to MongoDB');
+
+      try {
+        const inputConnectorData=[
+          [{
+            type: 'A',
+            wattage: 100,
+            manufacturer: 'Manufacturer1',
+          },
+          {
+            type: 'B',
+            wattage: 100,
+            manufacturer: 'Manufacturer1',
+          },
+          {
+            type: 'C',
+            wattage: 100,
+            manufacturer: 'Manufacturer1',
+          },
+          ],
+          [
+            {
+              type: 'AA',
+              wattage: 10,
+              manufacturer: 'Manufacturer2',
+            },
+            {
+              type: 'BB',
+              wattage: 10,
+              manufacturer: 'Manufacturer2',
+            },
+          ],
+        ];
+        await insertAll('MUMBAI', 'GAIL', inputConnectorData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
--- insertFunctions.js
+++ insertFunctions.js
@@ -1,30 +1,47 @@
-const { Connector, ChargePoint, Location } = require('./infrastructureSchema');
+const {Connector, ChargePoint, Location} = require('./infrastructureSchema');
 
-async function createConnectors(connectorsData){
-
-    return await Connector.insertMany(connectorsData);
+async function createConnectors(connectorsData) {
+  return await Connector.insertMany(connectorsData);
 }
 
-async function createChargePoints(connectors){
-
-   const chargePointConnectorArray={
-        connectors:connectors.map((connector)=>{
-         return connector._id;
-        })
-};
-   return await ChargePoint.create(chargePointConnectorArray);
+async function createChargePoints(connectors) {
+  const chargePointConnectorArray={
+    connectors: connectors.map((connector)=>{
+      return connector._id;
+    }),
+  };
+  return await ChargePoint.create(chargePointConnectorArray);
 }
 
-async function createLocation(chargePoints,locationName,chargeStationName){
-    
-    const locationData={
-        locationName : locationName,
-        chargingStationName:chargeStationName,
-        chargePoints: chargePoints.map((chargePoint)=>{
-            return chargePoint._id;
-        })
-    };
-    return await Location.create(locationData);
-
+async function createLocation(chargePoints, locationName, chargeStationName) {
+  const locationData={
+    locationName: locationName,
+    chargingStationName: chargeStationName,
+    chargePoints: chargePoints.map((chargePoint)=>{
+      return chargePoint._id;
+    }),
+  };
+  return await Location.create(locationData);
 }
-module.exports={createChargePoints,createConnectors,createLocation};
+
+
+async function insertAll( locationName, chargeStationName, dataInput) {
+  try {
+    const chargePointsArray = [];
+
+    for (const chargePoint of dataInput) {
+      const connectorsData=chargePoint.map((connector)=>{
+        return connector;
+      });
+      const connectorsArray = await createConnectors(connectorsData);
+      const createdChargePoint = await createChargePoints(connectorsArray);
+      chargePointsArray.push(createdChargePoint);
+    }
+    const location =await createLocation(chargePointsArray, locationName, chargeStationName);
+    return location;
+  } catch (error) {
+    console.error('Error creating location with charge points and connectors:', error);
+    throw error;
+  }
+}
+module.exports={insertAll};
